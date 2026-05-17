801ca8a1d65f75d9caaabd2dda0d21433814a99c
akpaciej <alekspaciej@gmail.com>
2026-05-17 17:37:01 -0500

    Delete 08_district_trends.sql

--- 08_district_trends.sql
+++
@@ -1,9 +0,0 @@
-
--- Crime trends by district --
-
-SELECT district,
-       primary_type,
-	   COUNT(*) AS total
-FROM chicago_crime
-GROUP BY district, primary_type
-ORDER BY district, total DESC;
